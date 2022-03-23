be307182a753e6df9c7140bd7cee0d2ab7875f01
GustavoDeveloper1 <devinvestidor02@gmail.com>
2022-03-23 17:07:50 -0300

    dark mode styled

--- src/ThemeDark.js
+++ src/ThemeDark.js
@@ -1,45 +1,10 @@
-//ThemeContext.js
-import React, { createContext, useState, useEffect } from 'react'
-
-export const ThemeContext = createContext()
-
-const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState('light')
-
-  useEffect(() => {
-    if (
-      localStorage.getItem('theme') === 'dark' ||
-      (!('theme' in localStorage) &&
-        window.matchMedia('(prefers-color-scheme: dark)').matches)
-    ) {
-      document.querySelector('html').classList.add('dark')
-      setTheme('dark')
-    } else {
-      document.querySelector('html').classList.remove('dark')
-      setTheme('light')
-    }
-  }, [])
-
-  function toggleThemeMode() {
-    if (
-      !localStorage.getItem('theme') ||
-      localStorage.getItem('theme') === 'light'
-    ) {
-      localStorage.theme = 'dark'
-      document.querySelector('html').classList.add('dark')
-      setTheme('dark')
-    } else {
-      localStorage.theme = 'light'
-      document.querySelector('html').classList.remove('dark')
-      setTheme('light')
-    }
-  }
-
-  return (
-    <ThemeContext.Provider value={{ theme, toggleThemeMode }}>
-      {children}
-    </ThemeContext.Provider>
-  )
+export const lightTheme = {
+  background: "#f6f8fd",
+  text: "#151620"
 }
 
-export default ThemeProvider
+export const darkTheme = {
+  background: "#151620",
+  text:"#f6f8fd"
+}
+
